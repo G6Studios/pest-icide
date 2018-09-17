3a05d6a8848f5088b64dc72d8930c5c89177b728
Quinn Daggett <quinn.daggett@uoit.net>
2018-09-16 20:10:34 -0400

    Version 0.2: Some actual content
    + Added a mouse controlled 3rd person camera
    + Added a simple ball as a player representation
    + Added 4 cubes to represent food the player can eat (complete with sound effects)
    + Added various scripts to facilitate the aforementioned actions

---
+++ PEST-icide/Assets/Scripts/characterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour {
+
+    public float speed = 10.0f; // Variable controlling how fast the player moves
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        // Pressing escape will unlock the cursor
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+	}
+}
---
+++ PEST-icide/Assets/Scripts/destroyOnTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOnTouch : MonoBehaviour {
+
+    bool enteredTrigger = false;
+    
+    void OnTriggerEnter(Collider coll)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        MeshRenderer mesh = GetComponent<MeshRenderer>();
+
+        if(coll.gameObject.tag == "player" && enteredTrigger == false)
+        {
+            audio.Play();
+            mesh.enabled = false;
+            enteredTrigger = true;
+        }
+    }
+}
