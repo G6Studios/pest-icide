56befa056e081fb9e4447cd68c56a7335ab0ccd6
Raza <raza.kazmi@uoit.net>
2018-09-18 21:48:20 -0400

    #controller input - added player movement and camera movement for the joysticks - need to have keyboard movement in at the same time

--- PEST-icide/Assets/Scripts/characterController.cs
+++ PEST-icide/Assets/Scripts/characterController.cs
@@ -4,21 +4,52 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour {
 
-    public float speed = 10.0f; // Variable controlling how fast the player moves
+    private Vector3 movementVector; //Movement vector for the player
+    private float speed = 10.0f; // Variable controlling how fast the player moves
+    private float jump = 15; //Players jump value
+    private float gravity = 40; 
 
 	// Use this for initialization
 	void Start () {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float strafe = Input.GetAxis("Horizontal") * speed;
-        translation *= Time.deltaTime;
-        strafe *= Time.deltaTime;
 
-        transform.Translate(strafe, 0, translation);
+        //float translation = Input.GetAxis("Vertical") * speed;
+        //  float strafe = Input.GetAxis("Horizontal") * speed;
+        // translation *= Time.deltaTime;
+        // strafe *= Time.deltaTime;
+
+        // transform.Translate(strafe, 0, translation);
+
+    
+       
+        //movementVector.x = Input.GetAxis("Vertical") * speed;
+        //movementVector.z = Input.GetAxis("Horizontal") * speed;
+        
+
+        movementVector.x = Input.GetAxis("LeftJoystickX") * speed;
+        movementVector.z = Input.GetAxis("LeftJoystickY") * speed;
+        movementVector.x *= Time.deltaTime;
+        movementVector.z *= Time.deltaTime;
+
+
+        /* if (character isGrounded)
+         {
+             movementVector.y = 0;
+
+             if (Input.GetButtonDown("A"))
+             {
+                 movementVector.y = jump;
+             }
+         }
+
+         movementVector.y -= (gravity * Time.deltaTime);
+         */
+
+        transform.Translate(movementVector.x, 0, movementVector.z);
 
         // Pressing escape will unlock the cursor
         if(Input.GetKeyDown("escape"))
