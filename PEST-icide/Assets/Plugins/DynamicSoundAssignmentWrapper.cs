1425b9a69606f76704c05ca69d2677e1e8be40a5
Jean Duchene <jeanpierre.duchene@uoit.net>
2018-11-10 14:56:00 -0500

    DLLupdate
    still getting error : DllNotFoundException: (DynamicSoundassignment) don't know how to fix it

--- PEST-icide/Assets/Plugins/DynamicSoundAssignmentWrapper.cs
+++ PEST-icide/Assets/Plugins/DynamicSoundAssignmentWrapper.cs
@@ -2,9 +2,9 @@
 using UnityEngine;
 using System.Runtime.InteropServices;
 
-public class DynamicSoundAssignmentWrapper : MonoBehaviour {
+public class DynamicSoundassignmentWrapper : MonoBehaviour {
 
-    const string DLL_NAME = "DynamicSoundAssignment";
+    const string DLL_NAME = "DynamicSoundassignment";
 
     [DllImport(DLL_NAME)]
     private static extern bool Init();
@@ -19,12 +19,21 @@ public class DynamicSoundAssignmentWrapper : MonoBehaviour {
     [DllImport(DLL_NAME)]
     private static extern void playSound();
 
-
-    // Update is called once per frame
-    void Update () {
-
+    // Use this for initialization 
+    void Start()
+    {
         initializeSound();
         loadSound(13);
+        print("playingsound");
+        playSound();
+        print("playedsound");
+
+    }
+
+
+        // Update is called once per frame
+    void Update () {
+       // print("playingsound");
         playSound();
     }
 }
