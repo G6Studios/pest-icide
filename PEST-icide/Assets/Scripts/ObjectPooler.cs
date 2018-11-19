2be2be44aad73d2f80da674f428b1436264784d8
Raza <raza.kazmi@uoit.net>
2018-11-19 04:22:38 -0500

    Added Dynamic Object Pool Shirinking
    +Object Timer script which can attach to any gameobject to keep track of how long its been inactive
    +Dynamic Object Pool Shrinking option for our object pools

--- PEST-icide/Assets/Scripts/ObjectPooler.cs
+++ PEST-icide/Assets/Scripts/ObjectPooler.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ObjectPooler : MonoBehaviour {
 
     //public static ObjectPooler current;
     public GameObject pooledObject; //gameobject we want pooled
     public int poolSize = 25;
     public bool growPool = true;  //if we want to grow our pool or not if it ends up full
+    public bool DynamicPoolShrinking = false;
+    public float DeletionTimeInSeconds = 15.0f;
 
     List<GameObject> objectPool;
 
@@ -16,14 +19,25 @@ public class ObjectPooler : MonoBehaviour {
     void Awake ()
     {
         objectPool = new List<GameObject>();
-        for (int i = 0; i < poolSize; i++)
+
+        for (uint i = 0; i < poolSize; i++)
         {
             GameObject ourObject = Instantiate(pooledObject);
             ourObject.SetActive(false);
+            ourObject.GetComponent<ObjectTimer>().SetDeactivatedTime();
             objectPool.Add(ourObject);
+            
+
         }
 
 	}
+    private void Start()
+    {
+        if (DynamicPoolShrinking)
+        {
+            InvokeRepeating("DeleteUselessPoolObj", DeletionTimeInSeconds + 1.0f, DeletionTimeInSeconds);
+        }
+    }
 
     //Method to get an inactive gameobject in our pool.
     public GameObject GetPooledObject()
@@ -42,12 +56,35 @@ public class ObjectPooler : MonoBehaviour {
             {
             GameObject ourObject = Instantiate(pooledObject);
                 ourObject.SetActive(false);
-             objectPool.Add(ourObject);
+                ourObject.GetComponent<ObjectTimer>().SetDeactivatedTime();
+                objectPool.Add(ourObject);
             }
             return objectPool[objectPool.Count - 1];
         }
         Debug.Log("No objects in pool available");
         return null;
+    }
+
+    private void DeleteUselessPoolObj()
+    {
+
+        for (int i = 0; i < objectPool.Count; i++)
+        {
+            if (!objectPool[i].activeInHierarchy) // We only want to check inactive pool objects
+            {
+                float deactivatedTime = objectPool[i].GetComponent<ObjectTimer>().GetDeactivatedTime();
+                if ((Time.time - deactivatedTime) >= DeletionTimeInSeconds)
+                {
+                    Destroy(objectPool[i]);
+                    objectPool.RemoveAt(i);
+                    i--; // Since our object pool size just got reduced, we need to reduce our i value to accomodate or else we wont delete correctly
+                    Debug.Log("Removed Object from pool from being inactive too long");
+                }
+            }
+
+        }
+
+
     }
 	
 
---
+++ PEST-icide/Assets/Scripts/ObjectTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Raza Kazmi 100592008
+//This class is for object timing.
+//We can use this to see how long an object has been active or deactive.
+//Can expand functionality later if desired, to see the object lifetime.
+
+public class ObjectTimer : MonoBehaviour
+{
+    private float timeDeactivated;
+
+    private void Start()
+    {
+        timeDeactivated = 0.0f;  
+    }
+
+    public float GetDeactivatedTime()
+    {
+        return timeDeactivated;
+    }
+
+    public void SetDeactivatedTime()
+    {
+        timeDeactivated = Time.time;
+    }
+
+    public void ResetDeactiveTime()
+    {
+        timeDeactivated = 0.0f;
+    }
+
+
+}
