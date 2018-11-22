2eceddc2320679cda8c1e5c4912e90ddda0b81ae
Quinn Daggett <quinn.daggett@uoit.net>
2018-11-22 17:54:58 -0500

    Beginning of Character ScriptableObjects
    + Characters are now generated from a character ScriptableObject
    * Information includes their mesh, collider, rigidbody, etc.
    * Still tweaking positions of collider and attacks

---
+++ PEST-icide/Assets/ScriptableObjects/Character.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject {
+
+    public string characterName; // Name of character
+    public float speed; // Movement speed of character
+    public float jumpHeight; // Jump height of character
+    public float jumpLength; // Jump length of character
+    public Mesh mesh; // Character mesh
+    public Collider collider; // The collider for the character
+    public Rigidbody rigidbody; // The rigidbody for the character
+    public Material material; // The material/texture for the character
+
+}
---
+++ PEST-icide/Assets/ScriptableObjects/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    public Character character; // The character scriptedobject
+
+    [SerializeField]
+    public int playerNumber; // The controller number that will be passed to the charactercontroller
+
+    // Variables handled by ScriptedObject
+    private string playerName;
+    private float speed;
+    private float jumpHeight;
+    private float jumpLength;
+    private Collider playerCollider;
+    private Rigidbody playerRigidbody;
+    private Mesh playerMesh;
+    private Material playerMaterial;
+
+    // Internal variables
+    private float resources;
+    private float invuln;
+    private Vector3 movementVector;
+    private float distToGround;
+
+	// Use this for initialization
+	void Start () {
+        loadCharacter();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadCharacter()
+    {
+        // Assigning values from scriptableobject
+        playerName = character.characterName;
+        speed = character.speed;
+        jumpHeight = character.jumpHeight;
+        jumpLength = character.jumpLength;
+        playerCollider = character.collider;
+        playerRigidbody = character.rigidbody;
+        playerMesh = character.mesh;
+        playerMaterial = character.material;
+
+        // Assigning values to gameobject components where necessary
+        gameObject.GetComponent<MeshFilter>().mesh = playerMesh;
+        gameObject.GetComponent<MeshRenderer>().material = playerMaterial;
+        gameObject.AddComponent<SphereCollider>();
+
+    }
+}
