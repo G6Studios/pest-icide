ebb9f780dd163058f9e4ad43b5c31ef9d7a31914
Raza Kazmi <raza.kazmi@uoit.net>
2018-11-26 15:45:25 -0500

    Objects now spawn only in the player area

--- PEST-icide/Assets/Scripts/ResourceSpawner.cs
+++ PEST-icide/Assets/Scripts/ResourceSpawner.cs
@@ -9,7 +9,24 @@ using UnityEngine;
 public class ResourceSpawner : MonoBehaviour {
 
     public GameObject plane;
+    public GameObject ExcludePlane1;
+    public GameObject ExcludePlane2;
+
     public ObjectPooler ResourcePool;
+
+    struct ObjectArea
+    {
+        public float minX, minZ, maxX, maxZ;
+
+        public ObjectArea(float minimumX, float minimumZ, float maximumX, float maximumZ)
+        {
+            minX = minimumX;
+            minZ = minimumZ;
+            maxX = maximumX;
+            maxZ = maximumZ;
+
+        }
+    }
 
     [DllImport("MemoryManagementPlugin")]
     public static extern void CreatePool(uint size); //create our pool with our size
@@ -44,11 +61,28 @@ public class ResourceSpawner : MonoBehaviour {
         float minZ = plane.GetComponent<MeshRenderer>().bounds.min.z;
         float maxZ = plane.GetComponent<MeshRenderer>().bounds.max.z;
 
+
+        float Ex1MinX = ExcludePlane1.GetComponent<MeshRenderer>().bounds.min.x;
+        float Ex1MinZ = ExcludePlane1.GetComponent<MeshRenderer>().bounds.min.z;
+        float Ex1MaxX = ExcludePlane1.GetComponent<MeshRenderer>().bounds.max.x;
+        float Ex1MaxZ = ExcludePlane1.GetComponent<MeshRenderer>().bounds.max.z;
+
+        float Ex2MinX = ExcludePlane2.GetComponent<MeshRenderer>().bounds.min.x;
+        float Ex2MinZ = ExcludePlane2.GetComponent<MeshRenderer>().bounds.min.z;
+        float Ex2MaxX = ExcludePlane2.GetComponent<MeshRenderer>().bounds.max.x;
+        float Ex2MaxZ = ExcludePlane2.GetComponent<MeshRenderer>().bounds.max.z;
+        
+
+        ObjectArea ExclusionPlane1 = new ObjectArea(Ex1MinX, Ex1MinZ, Ex1MaxX, Ex1MaxZ);
+        ObjectArea ExclusionPlane2 = new ObjectArea(Ex2MinX, Ex2MinZ, Ex2MaxX, Ex2MaxZ);
+
+
+
         CreatePool(100); // this is our pool for our vectors
 
         for (int i = 0; i < 100; i++)
         {
-            RandomizeVectorPool(minX,maxX,minZ,maxZ);
+            RandomizeVectorPool(minX,maxX,minZ,maxZ,ExclusionPlane1,ExclusionPlane2);
         }
 
         for (int i = 0; i < 20; i++)
@@ -88,14 +122,15 @@ public class ResourceSpawner : MonoBehaviour {
 
     }
 
-    void RandomizeVectorPool(float minX, float maxX, float minZ, float maxZ)
+    void RandomizeVectorPool(float minX, float maxX, float minZ, float maxZ, ObjectArea ExclusionPlane1, ObjectArea ExclusionPlane2)
     {
-        
+
+
         float RandomX = UnityEngine.Random.Range(minX, maxX);
         float RandomZ = UnityEngine.Random.Range(minZ, maxZ);
         Vector3 RandomLocation = new Vector3(RandomX, 2, RandomZ);
-        while (Physics.CheckSphere(RandomLocation, 0.5f)) // This line will break the spawning if it ends up coliding with the floor (Make sure radius will not collide with the floor).
-            {
+        while (Physics.CheckSphere(RandomLocation, 0.5f) || isInExclusionZone(RandomLocation,ExclusionPlane1) || isInExclusionZone(RandomLocation,ExclusionPlane2)) // This line will break the spawning if it ends up coliding with the floor (Make sure radius will not collide with the floor).
+        {
             RandomX = UnityEngine.Random.Range(minX, maxX);
             RandomZ = UnityEngine.Random.Range(minZ, maxZ);
             RandomLocation.x = RandomX;
@@ -104,9 +139,25 @@ public class ResourceSpawner : MonoBehaviour {
         //Vector3 randomized = new Vector3(UnityEngine.Random.Range(minX, maxX), 1, UnityEngine.Random.Range(minZ, maxZ));
         
         //AddToPool(UnityEngine.Random.Range(minX, maxX), 1, UnityEngine.Random.Range(minZ, maxZ));
-        AddToPool(RandomLocation.x, 1, RandomLocation.z);
+        AddToPool(RandomLocation.x, 1.5f, RandomLocation.z);
 
 
     }
+
+    bool isInExclusionZone(Vector3 Object, ObjectArea plane)
+    {
+        //if ((RandomLocation.x > ExclusionPlane1.minX) && (RandomLocation.x < ExclusionPlane1.maxX)
+        // && (RandomLocation.z > ExclusionPlane1.minZ) && (RandomLocation.z < ExclusionPlane1.maxZ))
+
+        if ((Object.x > plane.minX) && (Object.x < plane.maxX) &&
+       (Object.z > plane.minZ) && (Object.z < plane.maxZ))
+        {
+            return true;
+        }
+        else
+            return false;
+
+    }
+
 
 }
