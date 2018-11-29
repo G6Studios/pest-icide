2b3ba09aee5efda1b68203a61646ebef2243bb34
Quinn Daggett <quinn.daggett@uoit.net>
2018-11-29 11:58:44 -0500

    Removing garbage files

--- VisualstudioC++/DynamicSound/DynamicSoundassignment/DynamicSoundassignment/LibSettings.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef DYNAMICSOUNDASSIGNMENT_EXPORTS
-#define LIB_API __declspec(dllexport)
-#elif DYNAMICSOUNDASSIGNMENT_IMPORTS
-#define LIB_API __declspec(dllimport)
-#else
-#define LIB_API
-#endif
