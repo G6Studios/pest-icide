8d6e8e205d268b4eae61644248779a1de3b83808
Quinn Daggett <quinn.daggett@uoit.net>
2019-01-06 19:36:50 -0500

    GamesCon Bug Squashing
    * Weird collision behaviours fixed (stove, TV set, ramps)
    * Resource collection bug fixed (resource collection pad would get stuck and constantly drain the player's resources)

--- PEST-icide/Assets/Scripts/ResourceDeposit.cs
+++ PEST-icide/Assets/Scripts/ResourceDeposit.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceDeposit : MonoBehaviour {
+public class ResourceDeposit : MonoBehaviour
+{
 
     private IEnumerator coroutine;
-	
+
     void OnTriggerEnter(Collider coll)
     {
         coroutine = DrainResource(coll.gameObject);
@@ -18,35 +19,34 @@ public class ResourceDeposit : MonoBehaviour {
         StopCoroutine(coroutine);
     }
 
+
     IEnumerator DrainResource(GameObject player)
     {
-        while (true)
+
+        if (player.tag == "Player1" && player.GetComponent<Player>().resources > 0)
         {
-
-            if(player.tag == "Player1" && player.GetComponent<Player>().resources > 0)
-            {
-                player.GetComponent<Player>().resources--;
-                player.GetComponent<Player>().depositedResources++;
-            }
-            else if(player.tag == "Player2" && player.GetComponent<Player>().resources > 0)
-            {
-                player.GetComponent<Player>().resources--;
-                player.GetComponent<Player>().depositedResources++;
-            }
-            else if(player.tag == "Player3" && player.GetComponent<Player>().resources > 0)
-            {
-                player.GetComponent<Player>().resources--;
-                player.GetComponent<Player>().depositedResources++;
-            }
-            else if(player.tag == "Player4" && player.GetComponent<Player>().resources > 0)
-            {
-                player.GetComponent<Player>().resources--;
-                player.GetComponent<Player>().depositedResources++;
-            }
-
-            yield return new WaitForSeconds(0.5f);
+            player.GetComponent<Player>().resources--;
+            player.GetComponent<Player>().depositedResources++;
+        }
+        else if (player.tag == "Player2" && player.GetComponent<Player>().resources > 0)
+        {
+            player.GetComponent<Player>().resources--;
+            player.GetComponent<Player>().depositedResources++;
+        }
+        else if (player.tag == "Player3" && player.GetComponent<Player>().resources > 0)
+        {
+            player.GetComponent<Player>().resources--;
+            player.GetComponent<Player>().depositedResources++;
+        }
+        else if (player.tag == "Player4" && player.GetComponent<Player>().resources > 0)
+        {
+            player.GetComponent<Player>().resources--;
+            player.GetComponent<Player>().depositedResources++;
         }
 
+        yield return new WaitForSeconds(0.5f);
     }
+
+
 
 }
