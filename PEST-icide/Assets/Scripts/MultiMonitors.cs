aac1db81d8e8918e0a8466eab71f0cbb833ff407
Raza <raza.kazmi@uoit.net>
2019-01-16 09:50:42 -0500

    Basic Health bar and multi-monitor support
    + Basic multi-monitor support
    + Health Bar
    + Bar depletes as player loses health

---
+++ PEST-icide/Assets/Scripts/MultiMonitors.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is to active multiple monitor display
+// It needs to be called once on the scene startup
+
+public class MultiMonitors : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the default display and is always on
+        // Check if there are additional displays and activate them
+        if (Display.displays.Length > 1)
+            Display.displays[1].Activate();
+        if (Display.displays.Length > 2)
+            Display.displays[2].Activate();
+        if (Display.displays.Length > 3)
+            Display.displays[3].Activate();
+    }
+
+}
