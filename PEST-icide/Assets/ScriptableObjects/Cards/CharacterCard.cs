46cf66cf3ff78b13b8f617ed67e78e1b3c3631eb
Raza <raza.kazmi@uoit.net>
2019-01-24 21:58:32 -0500

    Additional character select development
    +Scriptable object character cards
    +UI Scaling fix
    +Placeholder character cards

---
+++ PEST-icide/Assets/ScriptableObjects/Cards/CharacterCard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterCard : ScriptableObject
+{
+
+    public string CharacterName;
+
+    public Sprite art;
+
+}
