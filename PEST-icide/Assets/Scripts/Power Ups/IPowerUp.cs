54258208458a471c4c06324e5bf09d7e9eadfe26
RazaKazmi <raza.kazmi@uoit.net>
2019-02-18 04:50:28 -0500

    Power Ups
    + Health Restore Orb Added
    + Restores 25 health on pickup

---
+++ PEST-icide/Assets/Scripts/Power Ups/IPowerUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Power up interface
+public interface IPowerUp
+{
+    void PickUp(Collider other);
+
+}
+
+//If our powerUp applies an effect for a duration
+// We need to create a coruntine function call and wrap it in pickup
+/*Ex.
+ * Void PickUp(Collider other)
+ * {
+ * Start Coruntine(TimedPickUp(other));
+ * }
+ * 
+ * IEnumerator TimedPickUp(Collider other)
+ * {
+ *  // Power up code here
+ *  // Disable Powerup collider here
+ *  // yield return new waitForSeconds()
+ *  // Destory gameobject
+ * }
+ * 
+ */
